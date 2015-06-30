92b0de9070e6bd094e35adf751000c1621c06621
Amaya713 <ehk31@case.edu>
2015-06-30 10:32:13 -0400

    first commit

---
+++ apps/Mobius Receiver/CMDTesting.js
@@ -0,0 +1,12 @@
+
+var sys = require('sys');
+
+var exec = require('child_process').exec, child;
+child = exec('java -jar DI.jar',
+  function (error, stdout, stderr){
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if(error !== null){
+      console.log('exec error: ' + error);
+    }
+});
